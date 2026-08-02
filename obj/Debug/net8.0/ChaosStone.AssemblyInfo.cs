69a1eb918a960c75e5cc1e41c84f64017ac4c864
cOuNtErSpElL <wellsalaister@gmail.com>
2026-08-02 11:35:15 -0400

    Added... whatever this is

--- obj/Debug/net8.0/ChaosStone.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChaosStone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c2738ae4b4db30bc257d3eaca14125d5e88b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
