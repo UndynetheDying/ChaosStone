7b7db404a9652a09428574d0e00993caaae3c156
cOuNtErSpElL <wellsalaister@gmail.com>
2026-09-05 21:26:23 -0400

    Updated the Bill Sword and added the Moon Minion

--- Content/Items/Weapons/BillSword.cs
+++ Content/Items/Weapons/BillSword.cs
@@ -3,44 +3,36 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ChaosStone.Content.Items.Weapons
-{
+{ 
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
 	public class BillSword : ModItem
 	{
-		public override void SetDefaults() {
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.ChaosStone.hjson' file.
+		public override void SetDefaults()
+		{
 			Item.damage = 15;
-			Item.knockBack = 4f;
-			Item.useStyle = ItemUseStyleID.Rapier; // Makes the player do the proper arm motion
-			Item.useAnimation = 12;
-			Item.useTime = 12;
+			Item.DamageType = DamageClass.Melee;
 			Item.width = 32;
 			Item.height = 32;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 6.5f;
+			Item.value = Item.buyPrice(silver: 10);
+			Item.rare = ItemRarityID.Blue;
 			Item.UseSound = SoundID.Item1;
-			Item.DamageType = DamageClass.MeleeNoSpeed;
-			Item.autoReuse = false;
-			Item.noUseGraphic = true; // The sword is actually a "projectile", so the item should not be visible when used
-			Item.noMelee = true; // The projectile will do the damage and not the item
-
-			Item.rare = ItemRarityID.White;
-			Item.value = Item.sellPrice(0, 0, 0, 10);
+			Item.autoReuse = true;
 		}
 
-		// Since this weapon is a projectile (uses noUseGraphic), it isn't naturally considered a melee weapon for the purposes of prefixes. This allows the expected prefixes to be applied.
-		public override bool MeleePrefix() => true;
-
-		/* Here is an example of using ApplyPrefix to apply item-specific tweaks to a specific prefix.
-		public override void ApplyPrefix(int pre) {
-			if(pre == PrefixID.Agile) {
-				Item.crit += 10;
-			}
-		}
-		*/
-
-		public override void AddRecipes() {
-			CreateRecipe()
-                .AddIngredient(ItemID.IronBar, 5)
-                .AddIngredient(ItemID.CopperShortsword, 1)
-				.AddTile(TileID.Anvils)
-				.Register();
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.IronBar, 10);
+			recipe.AddIngredient(ItemID.CopperShortsword, 1);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
 		}
 	}
 }
