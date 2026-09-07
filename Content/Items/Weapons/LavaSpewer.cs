f25141304b4f57579a29fdcb78f58427cd5d1a72
cOuNtErSpElL <wellsalaister@gmail.com>
2026-09-07 17:04:16 -0400

    Added more things

---
+++ Content/Items/Weapons/LavaSpewer.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChaosStone.Content.Items.Weapons
+{
+	public class LavaSpewer : ModItem
+	{
+		public override void SetDefaults() {
+            Item.damage = 10;
+            Item.DamageType = DamageClass.Magic;
+            Item.knockBack = 1f;
+			Item.width = 40;
+			Item.height = 40;
+			Item.UseSound = SoundID.Item71;
+            Item.shoot = ModContent.ProjectileType<Projectiles.Chaos>();
+            Item.shootSpeed = 5;
+            Item.useAmmo = AmmoID.None;
+			Item.SetShopValues(ItemRarityColor.LightRed4, 10000);
+            Item.mana = 15;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+                .AddIngredient(ItemID.IronBar, 5)
+				.AddTile(TileID.Anvils)
+				.Register();
+		}
+
+		public override void ModifyManaCost(Player player, ref float reduce, ref float mult) {
+			// We can use ModifyManaCost to dynamically adjust the mana cost of this item, similar to how Space Gun works with the Meteor armor set.
+			// See ExampleHood to see how accessories give the reduce mana cost effect.
+			if (player.statLife < player.statLifeMax2 / 2) {
+				mult *= 0.5f; // Half the mana cost when at low health. Make sure to use multiplication with the mult parameter.
+			}
+		}
+	}
+}
---
+++ Content/Projectiles/LavaSpewerProjectile.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChaosStone.Content.Projectiles
+{
+	public class LavaSpewerProjectile : ModProjectile
+	{
+		public override void SetDefaults() {
+			Projectile.width = 5; // The width of projectile hitbox
+			Projectile.height = 5; // The height of projectile hitbox
+
+			// Copy the ai of any given projectile using AIType, since we want
+			// the projectile to essentially behave the same way as the vanilla projectile.
+			AIType = ProjectileID.GoldenShowerFriendly;
+
+			Projectile.friendly = true; // Can the projectile deal damage to enemies?
+			Projectile.DamageType = DamageClass.Magic; // Is the projectile shoot by a ranged weapon?
+			Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+			Projectile.tileCollide = false; // Can the projectile collide with tiles?
+			Projectile.timeLeft = 60; // Each update timeLeft is decreased by 1. Once timeLeft hits 0, the Projectile will naturally despawn. (60 ticks = 1 second)
+
+			Projectile.penetrate = -1;
+			// 1: Projectile.penetrate = 1; // Will hit even if npc is currently immune to player
+			// 2a: Projectile.penetrate = -1; // Will hit and unless 3 is use, set 10 ticks of immunity
+			// 2b: Projectile.penetrate = 3; // Same, but max 3 hits before dying
+			// 5: Projectile.usesLocalNPCImmunity = true;
+			// 5a: Projectile.localNPCHitCooldown = -1; // 1 hit per npc max
+			// 5b: Projectile.localNPCHitCooldown = 20; // 20 ticks before the same npc can be hit again
+		}
+
+		// See comments at the beginning of the class
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+			// 3a: target.immune[Projectile.owner] = 20;
+			// 3b: target.immune[Projectile.owner] = 5;
+		}
+	}
+}
