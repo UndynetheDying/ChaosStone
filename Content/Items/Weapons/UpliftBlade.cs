033114d82991c557134b545df8c3e2ed39d8312e
UndynetheDying <wellsalaister@gmail.com>
2026-09-14 07:55:47 -0400

    Making the Uplift Blade

---
+++ Content/Items/Weapons/UpliftBlade.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChaosStone.Content.Items.Weapons
+{ 
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+	public class UpliftBlade : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.ChaosStone.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.damage = 70;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 6;
+			Item.value = Item.buyPrice(silver: 10);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = ProjectileID.CultistBossFireBallClone;
+		}
+
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient(ItemID.IronBar, 10)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+	}
+}
