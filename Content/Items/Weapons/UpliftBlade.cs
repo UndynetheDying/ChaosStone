c296aa6ae1dac21f60f41537800bee2de8a789d5
UndynetheDying <wellsalaister@gmail.com>
2026-09-15 13:10:32 -0400

    Updated Uplift

--- Content/Items/Weapons/UpliftBlade.cs
+++ Content/Items/Weapons/UpliftBlade.cs
@@ -35,28 +35,26 @@ namespace ChaosStone.Content.Items.Weapons
 				.Register();
 		}
 
-        if (!player.HasBuff(ModContent.BuffType<UpliftDebuff>()))
-        {
-            public override bool AltFunctionUse(Player player) {
-                return true;
-            }
 
-            public override bool? UseItem(Player player) {
+        public override bool AltFunctionUse(Player player) {
+            return true;
+        }
+
+        public override bool? UseItem(Player player) {
                 // Check if this specific use action is the right-click
-                if (player.altFunctionUse == 2)
-                {
-                    // A negative Y value moves the player UPWARD.
-                    // -15f is a sharp jump. Adjust this value to change the intensity.
-                    player.velocity.Y = -15f; 
-                    player.AddBuff(ModContent.BuffType<UpliftDebuff>(), 180); // 3 seconds
+            if (player.altFunctionUse == 2)
+            {
+                // A negative Y value moves the player UPWARD.
+                // -15f is a sharp jump. Adjust this value to change the intensity.
+                player.velocity.Y = -15f; 
+                player.AddBuff(ModContent.BuffType<UpliftDebuff>(), 180); // 3 seconds
 
-                    if (Main.netMode == NetmodeID.MultiplayerClient)
-                    {
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                {
                         NetMessage.SendData(MessageID.PlayerControls, number: player.whoAmI);
-                    }
                 }
-                return true;
             }
+            return true;
         }
 	}
 }
--- obj/Debug/net8.0/ChaosStone.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChaosStone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033114d82991c557134b545df8c3e2ed39d8312e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec27b8dde0230c11462aba8c4f86bc2cd1e9615")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChaosStone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
