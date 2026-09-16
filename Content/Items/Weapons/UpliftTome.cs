cb6b5c68f62d65b304710519b8970918dd7fdfcc
UndynetheDying <wellsalaister@gmail.com>
2026-09-16 10:27:04 -0400

    Updated Uplift

--- Content/Items/Weapons/UpliftTome.cs
+++ Content/Items/Weapons/UpliftTome.cs
@@ -5,21 +5,20 @@ using Terraria.ModLoader;
 
 namespace ChaosStone.Content.Items.Weapons
 {
-	public class ThieveryTome : ModItem
+	public class UpliftTome : ModItem
 	{
 		public override void SetDefaults() {
-			// DefaultToStaff handles setting various Item values that magic staff weapons use.
-			// Hover over DefaultToStaff in Visual Studio to read the documentation!
-			// Shoot a black bolt, also known as the projectile shot from the onyx blaster.
 			Item.width = 34;
 			Item.height = 40;
 			Item.UseSound = SoundID.Item71;
-
-			// A special method that sets the damage, knockback, and bonus critical strike chance.
-			// This weapon has a crit of 32% which is added to the players default crit chance of 4%
-			Item.SetWeaponValues(10, 20, 5);
-
 			Item.SetShopValues(ItemRarityColor.LightRed4, 10000);
+			Item.damage = 70;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.shoot = ProjectileID.SnowBallFriendly;
+			Item.shootSpeed = 20;
+			Item.mana = 15;
+			Item.useStyle = ItemUseStyleID.Shoot;
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
