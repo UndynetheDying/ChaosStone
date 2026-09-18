96beec07ea63b02e45c394c13cd6f13a50a419f4
UndynetheDying <wellsalaister@gmail.com>
2026-09-18 07:55:11 -0400

    Updated Uplift

--- Content/Items/Weapons/UpliftWhip.cs
+++ Content/Items/Weapons/UpliftWhip.cs
@@ -14,7 +14,7 @@ namespace ChaosStone.Content.Items.Weapons
 		public override void SetDefaults() {
 			// This method quickly sets the whip's properties.
 			// Mouse over to see its parameters.
-			Item.DefaultToWhip(ModContent.ProjectileType<UpliftWhipProjectile>(), 60, 2, 4);
+			Item.DefaultToWhip(ModContent.ProjectileType<UpliftWhipProjectile>(), 50, 2, 4);
 			Item.rare = ItemRarityID.Green;
 			Item.channel = true;
 		}
